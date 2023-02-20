2befd29e2c358e4635680adc44770ca66fb082de
LautaroZalazar <lautyzalazar10@gmail.com>
2023-02-20 16:38:33 -0300

    [Feat] detail router

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,7 @@ import userRouter from "./user.js";
 import productRouter from "./product.js";
 import ordersRouter from "./orders.js";
 import categoryRouter from "./category.js";
+import detailRouter from "./detail.js";
 
 const router = Router()
 
@@ -10,5 +11,6 @@ router.use("/user", userRouter)
 router.use("/product", productRouter)
 router.use("/orders", ordersRouter)
 router.use("/category", categoryRouter)
+router.use("/detail", detailRouter)
 
 export default router
